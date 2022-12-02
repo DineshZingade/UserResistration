a829fa25560d6a10d59c27836801fe019dbb919f
DineshZingade <zingadedinesh1991@gmail.com>
2022-12-02 22:24:11 +0530

    Added:User Password with atleast Numeric value

--- src/com/bridgelabz/UserResistration.java
+++ src/com/bridgelabz/UserResistration.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserResistration {
-	public void passWordRule2() {
+	public void passWordRule3() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter password :  ");
 		String passWord = sc.nextLine();
-		boolean check = Pattern.matches("[A-Z]{1}[a-z]{7,}", passWord);
+		boolean check = Pattern.matches("[A-Z]{1}[a-z]{6,}[0-9]{1}", passWord);
 		if (check)
-			System.out.println("This Password is valid");
+			System.out.println("Perfect! password");
 		else
 			System.out.println("Please Enter a Valid password");
 	}
@@ -19,7 +19,7 @@ public class UserResistration {
 		Scanner sc = new Scanner(System.in);
 		UserResistration user = new UserResistration();
 		System.out.println("Welcome to User Registration Program");
-		user.passWordRule2();
+		user.passWordRule3();
 
 	}
 }

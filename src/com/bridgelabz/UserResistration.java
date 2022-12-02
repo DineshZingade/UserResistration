a527741b4133db9eb4c470d6d1cdf58c839758e9
DineshZingade <zingadedinesh1991@gmail.com>
2022-12-02 10:06:08 +0530

    Added:Mobile Number

--- src/com/bridgelabz/UserResistration.java
+++ src/com/bridgelabz/UserResistration.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserResistration {
-	public void email() {
+	public void mobileNum() {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter Email of User: ");
-		String eMail = sc.next();
-		boolean check = Pattern.matches("^[a-z]*([.]?[a-z]+)*@bl[.]co([.]?in)*", eMail);
+		System.out.print("Enter Mobile number with country code (91)  : ");
+		String mobileNum = sc.nextLine();
+		boolean check = Pattern.matches("^91\\s[789][0-9]{9}", mobileNum);
 		if (check)
-			System.out.println("Perfect! Email!");
+			System.out.println("Perfect! Mobile Number");
 		else
-			System.out.println("Please Enter a Valid Email");
+			System.out.println("Please Enter a Valid Mobile Number");
 	}
 
 	public static void main(String[] args) {
 		UserResistration user = new UserResistration();
 		System.out.println("Welcome to User Registration Program");
-
-		user.email();
+		user.mobileNum();
 	}
 }
